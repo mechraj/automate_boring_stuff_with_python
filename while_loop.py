e28c08b3a8567858e14f7f5f48c76c49e232165b
mechraj <94051997+mechraj@users.noreply.github.com>
2022-11-24 22:38:12 +0400

    Creates a file while_loop.py
    program to understand working of while loop

---
+++ while_loop.py
@@ -0,0 +1,7 @@
+#Program to understand while loop
+
+name='' #Assigns a variable to empty string 
+while name!='my name':     #initializng while loop
+    print('Enter your name..?')    #Asks user for a name
+    name=input()
+print('Thank you.!')
