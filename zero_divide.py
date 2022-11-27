f1a8242a26f120a4b6ced593f7d72a2a6a6f7f9a
mechraj <94051997+mechraj@users.noreply.github.com>
2022-11-27 13:55:20 +0400

    Creates zero_divide.py
    An erroe prone program, which has a bug of zero division error

---
+++ zero_divide.py
@@ -0,0 +1,10 @@
+#program for error handling
+
+#function divides 100 by given argument
+def spam(divideby):
+    return 100/divideby
+
+print(spam(10))
+print(spam(4))
+print(spam(0))
+print(spam(1000))
