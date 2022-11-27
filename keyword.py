e75536efa7064b1a659b72a4e92aea2bd478f6b5
mechraj <94051997+mechraj@users.noreply.github.com>
2022-11-27 12:04:21 +0400

    Creates keyword.py
    A program to understand keywords

---
+++ keyword.py
@@ -0,0 +1,13 @@
+#program with keywords arguments
+
+print ('Hello')
+print('world')
+
+print('Hello',end=' ') #Adds end keyword to replace newline character in print
+print('world')
+
+print('How','are','you')
+
+print('How','are','you', sep=',')
+
+
