cd3941b3f781d64b5babd56d50c004ded10de040
mechraj <94051997+mechraj@users.noreply.github.com>
2022-11-27 12:17:01 +0400

    Update eggs.py
    Adds a global variable

--- eggs.py
+++ eggs.py
@@ -1,12 +1,8 @@
 #program to understand scope
 
 def spam():  #function is initialized
-    eggs=99
-    bacon()  #calling another function inside a function
     print(eggs)
 
-def bacon():  
-    ham=101
-    eggs=500
-
+eggs=99 #global variable
 spam()
+print(eggs)
