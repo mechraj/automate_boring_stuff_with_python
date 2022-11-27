d7e6d52d334d27ee2abb87ee48a61033cc7994b4
mechraj <94051997+mechraj@users.noreply.github.com>
2022-11-27 14:00:37 +0400

    Adds try and except statements
    Exception is added to handle the zerodivisionerroe

--- zero_divide.py
+++ zero_divide.py
@@ -1,8 +1,11 @@
 #program for error handling
 
 #function divides 100 by given argument
-def spam(divideby):
-    return 100/divideby
+def spam(divideby): 
+    try:    #since dividing number is prone to mathematical errors we use try statemnt
+        return 100/divideby
+    except ZeroDivisionError:   #exception for zero division error
+        print("Dividing a number with zero results in infinity.")
 
 print(spam(10))
 print(spam(4))
