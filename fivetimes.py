76d7905bbee3460e51bd2bc9b8c4a72229807aaa
mechraj <94051997+mechraj@users.noreply.github.com>
2022-11-27 10:02:08 +0400

    Examples of for loop and function

---
+++ fivetimes.py
@@ -0,0 +1,4 @@
+#An example tprogram to understand working of for loop
+for i in range(5):
+    print('My name is Jimmy (' + str(i)+')')
+    
---
+++ hellofunc.py
@@ -0,0 +1,9 @@
+#Progarm to understand function
+def hello():       #defines a function
+    print('Howdy..!')        
+    print('Hello')
+    print('Hey there..!')
+
+    
+hello()      #calls the function
+hello()
