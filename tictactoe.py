d8b98874760f0ab065ea291127a19460ed77cf07
mechraj <94051997+mechraj@users.noreply.github.com>
2022-12-18 11:49:29 +0400

    Creates a function to print the board to the screen

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,9 @@
 #A game of tictactoe
+#steps : 
+#Create a dictionary to represent to tictactoe board
+#Create a function to print tictactoe borad everytimr it is updated.
+#Create a loop which asks users X and O to enter in a space.
+
 
 #Creating a dictionary to represent a tictactoe board
 
@@ -6,3 +11,11 @@ the_board={'top-L':' ','top-M':' ','top-r':' ',
             'mid-L':' ', 'mid-M':' ', 'mid-R':' ',
             'bot-L':' ', 'bot-M':' ', 'bot-R':' '
             }
+
+# creating function to print the board
+def printboard(board):
+    print((board['top-L'])+' | '+(board['top-M'])+' | '+ (board['top-R']))
+    print('- + - + -')
+    print((board['mid-L'])+' | '+(board['mid-M'])+' | '+ (board['mid-R']))
+    print('- + - + -')
+    print((board['bot-L'])+' | '+(board['bot-M'])+' | '+ (board['bot-R']))
