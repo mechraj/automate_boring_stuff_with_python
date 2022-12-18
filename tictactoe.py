70a40bde63b6fff54954e0bd9a3ea23afc6b561b
mechraj <94051997+mechraj@users.noreply.github.com>
2022-12-18 11:36:50 +0400

    Creates tictactoe
    Creates a dictionary representing tictactoe board

--- tictactoe.py
+++ tictactoe.py
@@ -1,32 +1,8 @@
-#program that simulates a tectactoe board
-#steps
-#1. create a dictionary to represent  tic tac toe board
-#2. assign 'x' and '0' values to user
-#3. update the dictionary everytime  user enters his move
+#A game of tictactoe
 
-#creating a dictionary to represent the board
-theboard={'top-l':' ', 'top-m':' ','top-r':' ',
-'mid-l':' ', 'mid-m':' ','mid-r':' ',
-'bot-l':' ', 'bot-m':' ','bot-r':' ',}
+#Creating a dictionary to represent a tictactoe board
 
-# function that prints the board
-def printboard(board):
-    print(board['top-l']+'|'+board['top-m']+'|'+board['top-r'])
-    print('-+-+-')
-    print(board['mid-l']+'|'+board['mid-m']+'|'+board['mid-r'])
-    print('-+-+-')
-    print(board['bot-l']+'|'+board['bot-m']+'|'+board['bot-r'])
-
-turn='X'
-for i in range(9):
-    printboard(theboard) #prints board
-    print('User '+ turn+'. Make your move')
-    move=input() 
-    theboard[move]=turn #updates the board
-
-    if turn=='X':
-        turn='0'
-    else:
-        turn='X'
-
-printboard(theboard)
+the_board={'top-L':' ','top-M':' ','top-r':' ',
+            'mid-L':' ', 'mid-M':' ', 'mid-R':' ',
+            'bot-L':' ', 'bot-M':' ', 'bot-R':' '
+            }
